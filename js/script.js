d0f7e00358050d1579c49afc87245de50d75bcc9
Bremelyn Duran <bremelynduran@ccpaedu.com>
2019-03-15 01:24:49 +0000

    button

--- js/script.js
+++ js/script.js
@@ -4,7 +4,18 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+  var searchTerm = $("input").val();
+
+    var request_url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    
+    $.ajax({
+      url: request_url,
+      method: "GET",
+      success: function(response) {
+           var pic_url = response.data[0].images.original.url;
+           $('body').append('<img src="' + pic_url + '">');
+      },
+    }); 
   
   
 });
